eee6f3fa6c396f1f51d027e897f4840a3e09a235
Josh McKinney <joshka@users.noreply.github.com>
2024-03-24 05:59:58 -0700

    Replace inline code with function for each field

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
+use itertools::Itertools;
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{self, parse_macro_input, Data, DeriveInput};
 
 #[proc_macro_derive(Widget)]
@@ -8,28 +9,35 @@ pub fn derive_widget(input: TokenStream) -> TokenStream {
     let name = input.ident;
     match input.data {
         Data::Struct(data) => {
+            let string_fields = data.fields.into_iter().filter(|field| {
+                matches!(&field.ty, syn::Type::Path(path) if path.path.segments[0].ident == "String")
+            }).collect_vec();
+
             // get the length of the longest field name so we can align the output
-            let max_label_length = data
-                .fields
+            let label_length = string_fields.iter().map(label_width).max().unwrap_or(0);
+
+            let functions = string_fields
                 .iter()
-                .map(|field| field.ident.as_ref().unwrap().to_string().len())
-                .max()
-                .unwrap_or(0);
-            let fields = data.fields.iter().map(|field| {
-                let name = &field.ident.as_ref().expect("struct fields must have names");
-                match &field.ty {
-                    syn::Type::Path(path) if path.path.segments[0].ident == "String" => {
-                        render_string_field(name, max_label_length)
-                    }
-                    _ => panic!("Widget derive only works on structs with string fields"),
+                .map(|field| render_field(field.ident.as_ref().unwrap(), label_length));
+
+            let function_calls = string_fields.iter().map(|field| {
+                let function_name = format_ident!("render_{}", field.ident.as_ref().unwrap());
+                quote! {
+                    let row = Rect::new(area.x, y, area.width, area.height - y);
+                    self.#function_name(row, buf);
+                    y += 1;
                 }
             });
             let expanded = quote! {
                 impl Widget for &#name {
                     fn render(self, area: Rect, buf: &mut Buffer) {
                         let mut y = area.y;
-                        #(#fields)*
+                        #(#function_calls)*
                     }
+                }
+
+                impl #name {
+                    #(#functions)*
                 }
             };
             TokenStream::from(expanded)
@@ -38,13 +46,22 @@ pub fn derive_widget(input: TokenStream) -> TokenStream {
     }
 }
 
-fn render_string_field(name: &syn::Ident, max_label_length: usize) -> proc_macro2::TokenStream {
+fn label_width(field: &syn::Field) -> usize {
+    field.ident.as_ref().unwrap().to_string().len()
+}
+
+/// Create a function to render a specific field
+fn render_field(name: &syn::Ident, label_length: usize) -> proc_macro2::TokenStream {
+    let function_name = format_ident!("render_{}", name);
+    let label = format!("{:>label_length$}: ", name);
+    let label_length = label.len() as u16;
     quote! {
-        let label = format!("{:>label_length$}:", stringify!(#name), label_length = #max_label_length);
-        let label = ratatui::text::Text::raw(label);
-        let value = ratatui::text::Text::raw(self.#name.to_string());
-        label.render(Rect { y, ..area }, buf);
-        value.render(Rect { y, x: area.x + #max_label_length as u16 + 2, ..area }, buf);
-        y += 1;
+        fn #function_name(&self, area: Rect, buf: &mut Buffer) {
+            const LABEL_WIDTH: u16 = #label_length;
+            let label_area = Rect::new(area.x, area.y, LABEL_WIDTH, 1);
+            let value_area = Rect::new(area.x + LABEL_WIDTH, area.y, area.width - LABEL_WIDTH, 1);
+            ratatui::text::Text::raw(#label).render(label_area, buf);
+            ratatui::text::Text::raw(self.#name.to_string()).render(value_area, buf);
+        }
     }
 }
